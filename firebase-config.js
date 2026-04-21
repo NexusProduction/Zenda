feaa7395fda17d94ced031ba0a96e8153e760adf
Nexus <buisness65201@gmail.com>
2026-04-21 09:55:18 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey:            "AIzaSyDKtQMG-fUr2rvGL1bgnnSbSrfaAVu9Nn4",
-  authDomain:        "zenda-app-fe1c2.firebaseapp.com",
-  projectId:         "zenda-app-fe1c2",
-  storageBucket:     "zenda-app-fe1c2.firebasestorage.app",
-  messagingSenderId: "764717910838",
-  appId:             "1:764717910838:web:18a9ac7772ba91f4928197",
-  measurementId:     "G-4HFMJ8DXPL"
+  apiKey:            "YOUR_NEW_API_KEY",
+  authDomain:        "YOUR_PROJECT.firebaseapp.com",
+  projectId:         "YOUR_PROJECT_ID",
+  storageBucket:     "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId:             "YOUR_APP_ID"
 };
 
 const app  = initializeApp(firebaseConfig);
@@ -20,9 +19,9 @@ export { auth, db };
 export { firebaseConfig };
 
 export const EMAILJS_CONFIG = {
-  serviceId: 'service_dc7ge9b',
+  serviceId:  'service_dc7ge9b',
   templateId: 'template_4turj9r',
-  publicKey: 'dg8t6B0d-DhPa3Tl6'
+  publicKey:  'dg8t6B0d-DhPa3Tl6'
 };
 
 export const APP_VERSION = "1.0.0";
