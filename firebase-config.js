34983a65962a8eedb0793164d081e87dd4c3218f
Nexus <buisness65201@gmail.com>
2026-07-17 12:54:55 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
-
 const firebaseConfig = {
   apiKey:            "AIzaSyArQLilynpEoydg-nye9yCQ2cQIVpeuyUI",
   authDomain:        "zenda-app-593a8.firebaseapp.com",
